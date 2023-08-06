e306560332db2056ad4f5b0f43249bb240b95650
unknown <ichissingue@outlook.com>
2023-08-06 06:59:34 +0200

    Estrutura inicial

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import styles from '../Components/Footer.module.css'
+
+const Footer = () =>{
+
+    return (
+        <>
+            <div className={styles.Footer}>Footer</div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import styles from '../Components/Header.module.css'
+
+const Header = () =>{
+
+    return (
+        <>
+            <div className={styles.Header}>Header</div>
+        </>
+    )
+}
+
+export default Header
