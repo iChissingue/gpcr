2fdc0ccc529cae976ee9fcc0bc9d4d39f7ca3f96
unknown <ichissingue@outlook.com>
2023-08-07 06:24:37 +0200

    Resolver o erro de envio de dado de login para api.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,27 +1,35 @@
 import Axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () =>{
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+   const usernameSet = useForm()
+   const passwordSet = useForm()
+   const url = 'http://localhost:2020/user/login'
 
-   const handdleSubmit = (e) =>{
+   const handdleSubmit = async (e) =>{
      e.preventDefault()
-        Axios.post('http://localhost:2020/user/login', {username, password}).then(response =>{
-            console.log(response)
-        })
+
+     if(usernameSet.validate() && passwordSet.validate()){
+        const {value: username} = usernameSet
+        const {value: password} = passwordSet 
+        const loginValues = {username, password}
         
-    }
+        await Axios.post(url, loginValues).then(response =>{
+             console.log(response.data)
+         })
+     }    
+   }
     return (
         <div>
             <h1>Login</h1>
             <form action=""onSubmit={handdleSubmit} >
-                <Input label="Usuario" type="text" name="username"/>
-                <Input label="Senha" type="password" name="password"/>   
-                <Button disabled>Entrar</Button>
+                <Input label="Usuario" type="text" name="username" {...usernameSet}/>
+                <Input label="Senha" type="password" name="password" {...passwordSet}/>   
+                <Button >Entrar</Button>
             </form>
             
             <Link to='/login/loginCreate'>Cria Usuario</Link>
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const useForm = () =>{
+    const [  value, setValue ] = useState('')
+    const [  error, setError ] = useState(null)
+
+    const validate =(value) =>{
+        if(value.length === 0){
+            setError("Preencha um valor!")
+            return false
+        }else{
+            setError(null)
+            return true
+        }
+    }
+
+    const onChange = ({target}) =>{
+        if(error)validate(target.value)
+        setValue(target.value)
+    }
+
+    return {
+        value,
+        setValue,
+        onChange,
+        error,
+        validate: () => validate(value),
+        onBlur: () => validate(value),
+    }
+}
+
+export default useForm
