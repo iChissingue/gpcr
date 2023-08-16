5029a92b0dcd7d63001b5898e8f84f542a0cf1d1
unknown <ichissingue@outlook.com>
2023-08-17 00:13:39 +0200

    Arranjos na janela de poupancas

--- src/pages/MemberHistory.js
+++ src/pages/MemberHistory.js
@@ -3,16 +3,20 @@ import { UserContext } from "../UserContext"
 
 const MemberHistory = () =>{
     const { memberSavings, memberData} = useContext(UserContext)
-
+    // const total = memberSavings.reduce((acc, curr) =>{
+    //     acc.savingsAmmount += acc + curr 
+    // },  )
+    //  console.log(total)
     return (
         <section className="container" style={{marginTop: 20}}>
             <h2>Lista de Poupancas</h2>
             {
-                memberSavings? memberSavings.map(savings => (
-                  <p style={{marginTop: 20}}>{`${savings.savingsAmmount},00MT - ${savings.sFund},00MT - ${savings.savingsDate}`}</p>  
+                memberSavings? memberSavings.map((savings, i) => (
+                  <p key={i}    style={{marginTop: 20}}>{`${savings.savingsAmmount},00MT - ${savings.sFund},00MT - ${savings.savingsDate}`}</p>  
                 ))
-                : <p style={{marginTop: 20}}>`O(A) Sr(a) ${memberData.name} ainda nao efetuou nenhuma poupanca!`</p>
+                : <p style={{marginTop: 20}}>O(A) Sr(a) {memberData.name} ainda nao efetuou nenhuma poupanca!</p>
             }
+            
         </section>
     )
 
